6ded0ceb7eb06f4a11df34b4e8396bc47e88a42a
MinecraftPEayer <boingli227@gmail.com>
2025-11-02 15:02:20 +0800

    init commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const fs = require('fs');
+
+(async function () {
+    const arcades = (await axios.get(process.env.ARCADE_LIST_URL)).data.map(a => a.id)
+
+    for (const arcade of arcades) {
+        const url = process.env.DATA_URL + arcade;
+        fs.mkdirSync('dist', { recursive: true });
+        const filePath = `dist/${arcade}.json`;
+
+        if (!url) {
+            throw new Error('Environment variable DATA_URL is not set');
+        }
+
+        try {
+            const response = await axios.get(url);
+            fs.writeFileSync(filePath, JSON.stringify(response.data, null, 2));
+            console.log(`Data saved to ${filePath}`);
+        } catch (error) {
+            throw new Error('Error fetching data:', error);
+        }
+    }
+})()
